1b532e8024dc3df20a2e8ac156b29ae06c5d7032
yashith <yashith98@gmail.com>
2022-07-13 22:26:00 +0530

    Cloud function for google sheets

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
-const cors = require("cors")({origin: true});
+const {google} = require("googleapis");
 admin.initializeApp();
 
 /**
@@ -10,8 +10,8 @@ admin.initializeApp();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "", //add email
-    pass: "", //add pass
+    user: "", // add email
+    pass: "", // add pass
   },
 });
 
@@ -34,4 +34,42 @@ exports.autoSendEmail = functions.firestore
           functions.logger.log(info);
         }
       });
+    });
+const spreadsheetId = "1w6ledz_-765y2q3uFOEmKaZslgmoxJxu8t0TQgSib5s";
+exports.populateSheet = functions.firestore
+    .document("/Signup details/{userId}")
+    .onCreate((snap, context) => {
+      const list=[];
+      const user = snap.data();
+      if (user) {
+        for (const item in user) {
+          if (typeof item !== "undefined") {
+            list.push(user[item]);
+          }
+        }
+      }
+      google.auth.getClient({
+        scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+      }).then((auth) => {
+        console.info("This auth obj"+JSON.stringify(auth));
+        const sheet = google.sheets({version: "v4", auth});
+        sheet.spreadsheets.values.append({
+          auth, // auth object
+          spreadsheetId, // spreadsheet id
+          range: "Sheet1!A:B", // sheet name and range of cells
+          valueInputOption: "USER_ENTERED",
+          resource: {
+            values: [list],
+          },
+        },
+        (err, response) => {
+          if (err) {
+            functions.logger.error(err);
+          }
+        }
+        );
+      })
+          .catch((err) => {
+            functions.logger.error(err);
+          });
     });
